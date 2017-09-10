1336d4bfbdf142ba1a9be0ea74cec87e014df1e8
Devon Wood <wooddv35@gmail.com>
2017-09-10 11:38:45 -0400

    finished core cloze card functionality

--- CLI.js
+++ CLI.js
@@ -5,32 +5,50 @@ var fs = require("fs");
 
 var param1 = process.argv[2];
 
-if (param1 == "basic" || param1 == "cloze") {
+if (param1 == "basic") {
 	inquirer.prompt([
 		{
 			name: "question",
 			type: "input",
-			message: "Create a new question"
+			message: "Create a new question:"
 		},
 		{
 			name: "answer",
 			type: "input",
-			message: "Create a new answer"
+			message: "Create a new answer:"
 		}
 	]).then(function(input) {
-		if (param1 == "basic") {
-			var newQuestion = new BasicCard(input.question, input.answer);
-			console.log(newQuestion.front);
-			console.log(newQuestion.back);
-			fs.appendFile("flashlog.txt", newQuestion.front + ', ' + newQuestion.back, function(err) {
-				if (err) {
-					return console.log(err);
-				} 
-			});
-		} else if (param1 == "cloze") {
-
-		}
+		var newQuestion = new BasicCard(input.question, input.answer);
+		console.log("Question: " + newQuestion.front);
+		console.log("Answer: " + newQuestion.back);
+		fs.appendFile("flashlog.txt", newQuestion.front + ', ' + newQuestion.back, function(err) {
+			if (err) {
+				return console.log(err);
+			} 
+		});
 	});
+} else if (param1 == 'cloze') {
+	inquirer.prompt([
+		{
+			name: "fullQuestion",
+			type: "input",
+			message: "Write cloze statement:"
+		},
+		{
+			name: "clozeAnswer",
+			type: "input",
+			message: "Write cloze answer:"
+		}
+	]).then(function(input) {
+		var newClozeQuestion = new ClozeCard(input.fullQuestion, input.clozeAnswer);
+		newClozeQuestion.clozeDelete();
+		if (input.fullQuestion == newClozeQuestion.partial) {
+			return console.log(input.clozeAnswer + ' is not part of the question');
+		}
+		console.log('Question: ' + newClozeQuestion.partial);
+		console.log('Answer: ' + newClozeQuestion.answer);
+	})
 } else {
 	console.log(param1 + " is not a valid command");
 }
+
--- ClozeCard.js
+++ ClozeCard.js
@@ -1,6 +1,11 @@
 var ClozeCard = function(text, cloze) {
-	this.text = text;
-	this.cloze = cloze;
+	this.fullText = text;
+	this.answer = cloze;
+	this.partial = '';
+}
+
+ClozeCard.prototype.clozeDelete = function() {
+	this.partial = this.fullText.replace(this.answer, '');
 }
 
 module.exports = ClozeCard;
